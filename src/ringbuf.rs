313a9cb6fe57eb2e397900d3fd460ad1aa1d37ec
Lochlann Andrews <lochlanna@gmail.com>
2022-12-09 14:00:35 +1000

    direct access bulk copy

--- src/ringbuf.rs
+++ src/ringbuf.rs
@@ -77,11 +77,36 @@ where
         }
         let start_idx = id % self.size;
         let end_idx = safe_head % self.size;
-        unsafe {
-            result.extend_from_slice(&((*ring)[start_idx..end_idx+1]))
+
+        if start_idx == end_idx {
+            return 0;
         }
 
-        end_idx - start_idx
+        return if end_idx > start_idx {
+            let num_elements = end_idx - start_idx + 1;
+            // result.reserve(num_elements);
+            let res_start = result.len();
+            unsafe {
+                result.set_len(res_start + num_elements);
+                let slice_to_copy = &((*ring)[start_idx..(end_idx + 1)]);
+                result[res_start..(res_start + num_elements)].copy_from_slice(slice_to_copy);
+            }
+            let last_id = result.last().expect("").1;
+            num_elements
+        } else {
+            let num_elements = end_idx + (self.size - start_idx) + 1;
+            // result.reserve(num_elements);
+            let mut res_start = result.len();
+            unsafe {
+                result.set_len(res_start + num_elements);
+                let slice_to_copy = &((*ring)[start_idx..]);
+                result[res_start..(res_start + slice_to_copy.len())].copy_from_slice(slice_to_copy);
+                res_start += slice_to_copy.len();
+                let slice_to_copy = &((*ring)[..end_idx + 1]);
+                result[res_start..(res_start + slice_to_copy.len())].copy_from_slice(slice_to_copy);
+            }
+            num_elements
+        }
     }
 
     pub fn head(&self) -> i64 {
@@ -100,11 +125,6 @@ mod tests {
     use std::time::Duration;
     use test::Bencher;
 
-    #[derive(Clone, Debug)]
-    struct Test {
-        b: bool,
-        c: i64,
-    }
     fn reader(ring: &Arc<RingBuffer<i32>>, size_hint: usize) -> JoinHandle<Vec<i32>> {
         let ring = ring.clone();
         let jh = thread::spawn(move || {
@@ -169,12 +189,30 @@ mod tests {
             jh
         };
 
+        let bulk_reader = |ring: &Arc<RingBuffer<i32>>, num_values: usize| {
+            let ring = ring.clone();
+            let jh = thread::spawn(move|| {
+                let mut current_id = 0;
+                let mut buffer = Vec::with_capacity(num_values);
+                loop {
+                    if ring.read_batch_from(current_id, &mut buffer) > 0 {
+                        let last = buffer.last().expect("");
+                        current_id = buffer.last().expect("couldn't get the last value").1 + 1;
+                    }
+                    if current_id >= num_values - 1 {
+                        return;
+                    }
+                }
+            });
+            jh
+        };
+
         let values: Vec<i32> = (0..100000).collect();
         let num_values = values.len();
         b.iter(|| {
             let ring = Arc::new(RingBuffer::new(4096));
-            let reader_jh_a = spin_reader(&ring, values.len());
-            let reader_jh_b = spin_reader(&ring, values.len());
+            let reader_jh_a = bulk_reader(&ring, values.len());
+            let reader_jh_b = bulk_reader(&ring, values.len());
             // thread::sleep(Duration::from_secs_f64(0.05));
             let writer_jh = writer(&ring, &values);
 
@@ -184,6 +222,52 @@ mod tests {
 
             reader_jh_b.join().expect("couldn't join reader!");
         });
+    }
+
+    #[test]
+    fn one_reader_one_writer_tesst() {
+        let spin_reader = |ring: &Arc<RingBuffer<i32>>, num_values: usize| {
+            let ring = ring.clone();
+            let jh = thread::spawn(move|| {
+                for id in 0..num_values {
+                    ring.try_get(id);
+                }
+            });
+            jh
+        };
+
+        let bulk_reader = |ring: &Arc<RingBuffer<i32>>, num_values: usize| {
+            let ring = ring.clone();
+            let jh = thread::spawn(move|| {
+                let mut current_id = 0;
+                let mut buffer = Vec::with_capacity(num_values);
+                loop {
+                    if ring.read_batch_from(current_id, &mut buffer) > 0 {
+                        let last = buffer.last().expect("");
+                        current_id = buffer.last().expect("couldn't get the last value").1 + 1;
+                    }
+                    if current_id >= num_values - 1 {
+                        return;
+                    }
+                }
+            });
+            jh
+        };
+
+        let values: Vec<i32> = (0..100000).collect();
+        let num_values = values.len();
+
+        let ring = Arc::new(RingBuffer::new(4096));
+        let reader_jh_a = bulk_reader(&ring, values.len());
+        // let reader_jh_b = bulk_reader(&ring, values.len());
+        // thread::sleep(Duration::from_secs_f64(0.05));
+        let writer_jh = writer(&ring, &values);
+
+        writer_jh.join().expect("couldn't join writer!");
+
+        reader_jh_a.join().expect("couldn't join reader!");
+
+        // reader_jh_b.join().expect("couldn't join reader!");
     }
 
     #[test]
@@ -232,10 +316,78 @@ mod tests {
         ring.put(3);
         ring.put(4);
         ring.put(5);
-        let mut vals = Vec::new();
+        let mut vals = Vec::with_capacity(5);
         ring.read_batch_from(0, &mut vals);
         let vals: Vec<i32> = vals.iter().map(|(v, i)|*v).collect();
         println!("got {:?}", vals);
         assert_eq!(vals, vec![1,2,3,4,5])
+    }
+
+    #[test]
+    fn bulk_read_from_middle() {
+        let ring = RingBuffer::new(20);
+        ring.put(-2);
+        ring.put(-1);
+        ring.put(0);
+        let mut vals = Vec::with_capacity(5);
+        ring.read_batch_from(0, &mut vals);
+        let mut last_id = vals.last().expect("Couldn't get the last value").1;
+        assert_eq!(last_id, 2);
+        last_id += 1;
+        ring.put(1);
+        ring.put(2);
+        ring.put(3);
+        ring.put(4);
+        ring.put(5);
+        let mut vals = Vec::with_capacity(5);
+        ring.read_batch_from(last_id, &mut vals);
+        let vals: Vec<i32> = vals.iter().map(|(v, i)|*v).collect();
+        println!("got {:?}", vals);
+        assert_eq!(vals, vec![1,2,3,4,5])
+    }
+
+    #[test]
+    fn bulk_read_wrap() {
+        let ring = RingBuffer::new(5);
+        ring.put(1);
+        ring.put(2);
+        ring.put(3);
+        ring.put(4);
+        ring.put(5);
+        ring.put(6);
+        ring.put(7);
+        ring.put(8);
+        let mut vals = Vec::new();
+        ring.read_batch_from(3, &mut vals);
+        let vals: Vec<i32> = vals.iter().map(|(v, i)|*v).collect();
+        println!("got {:?}", vals);
+        assert_eq!(vals, vec![4,5,6,7,8])
+    }
+
+    #[test]
+    fn bulk_read_wrap_from_middle() {
+        let ring = RingBuffer::new(10);
+        ring.put(-3);
+        ring.put(-2);
+        ring.put(-1);
+        ring.put(0);
+        let mut vals = Vec::with_capacity(10);
+        ring.read_batch_from(0, &mut vals);
+        let mut last_id = vals.last().expect("Couldn't get the last value").1;
+        assert_eq!(last_id, 3);
+        last_id += 1;
+        ring.put(1);
+        ring.put(2);
+        ring.put(3);
+        ring.put(4);
+        ring.put(5);
+        ring.put(6);
+        ring.put(7);
+        ring.put(8);
+        let mut vals = Vec::with_capacity(10);
+        ring.read_batch_from(last_id, &mut vals);
+        let vals: Vec<i32> = vals.iter().map(|(v, i)|*v).collect();
+        println!("got {:?}", vals);
+        assert_eq!(vals, vec![1,2,3,4,5,6,7,8])
     }
 }
